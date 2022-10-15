25b97fe845a18351b0a29c1f6e18068756c622ad
Thisal <dinukathisal.1995@gmail.com>
2022-10-15 12:16:29 +0530

    Initial commit

---
+++ src/main/java/com/dinuka/ryanair/RyanairInterconnectingFlightsApplication.java
@@ -0,0 +1,12 @@
+package com.dinuka.ryanair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyanairInterconnectingFlightsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RyanairInterconnectingFlightsApplication.class, args);
+  }
+}
---
+++ src/test/java/com/dinuka/ryanair/RyanairInterconnectingFlightsApplicationTests.java
@@ -0,0 +1,11 @@
+package com.dinuka.ryanair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyanairInterconnectingFlightsApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
